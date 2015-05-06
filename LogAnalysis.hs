bd97bce9a19d1b53cb45fbde71f2049b4246cc00
mario <mjh@protonmail.ch>
2015-05-06 14:32:24 -0500

    I think I'm done

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -20,32 +20,27 @@ string2Log ("I":y:zs) = LogMessage Info (read y) (unwords zs)
 string2Log ("W":y:zs) = LogMessage Warning (read y) (unwords zs)
 string2Log _ = Unknown "random"
 
-insert' :: LogMessage -> MessageTree -> MessageTree -- checks out
-insert' x (Node z y w)
-  | (log2Time x) > (log2Time y) = (Node z y (insert' x w))
-  | (log2Time x) < (log2Time y) = (Node (insert' x z) y w)
+insert :: LogMessage -> MessageTree -> MessageTree -- checks out
+insert x (Node z y w)
+  | (log2Time x) > (log2Time y) = (Node z y (insert x w))
+  | (log2Time x) < (log2Time y) = (Node (insert x z) y w)
   | otherwise = (Node z y w) 
-insert' (Unknown _) y = y
-insert' x _ = (Node Leaf x Leaf)
+insert (Unknown _) y = y
+insert x _ = (Node Leaf x Leaf)
 
 log2Time :: LogMessage -> TimeStamp
 log2Time (LogMessage _ n _) = n
 log2Time (Unknown _) = 0
 
-tree2Log :: MessageTree -> LogMessage
-tree2Log (Node _ n _) = n
-tree2Log (Leaf) = (Unknown "what")
-
 build :: [LogMessage] -> MessageTree -- checks out
-build (x:xs) = insert' x (build xs)
+build (x:xs) = insert x (build xs)
 build [] = (Leaf)
 
-inOrder :: MessageTree -> [LogMessage]
-inOrder x = (tree2Log x):(inOrder x)
 
-inOrder' :: MessageTree -> [LogMessage]
-inOrder' (Node m n o)
-  | m == (Node _ _ _) = inOrder' m
+inOrder :: MessageTree -> [LogMessage]
+inOrder (Node m n o) = (inOrder m) ++ [n] ++ (inOrder o)
+inOrder (Leaf) = []
+
 
 tree2Time :: MessageTree -> TimeStamp
 tree2Time (Node _ n _) = log2Time n
@@ -56,15 +51,16 @@ errorsAbove ((LogMessage (Error x) y z):ls)
   | x > 25 = ((LogMessage (Error x) y z):(errorsAbove ls))
   | x < 25 = [] ++ (errorsAbove ls)
   | otherwise = [(LogMessage Warning 59 "otherwise")] ++ (errorsAbove ls)
-perrorsAbove ((LogMessage Info _ _):ls) = [] ++ (errorsAbove ls)
+errorsAbove ((LogMessage Info _ _):ls) = [] ++ (errorsAbove ls)
 errorsAbove ((LogMessage Warning _ _):ls) = [] ++ (errorsAbove ls)
+errorsAbove ((Unknown _):ls) = [] ++ (errorsAbove ls)
 errorsAbove _ = []
 
 
 sortByTimeGetText :: [LogMessage] -> [String]
 sortByTimeGetText ((LogMessage _ x z):(LogMessage _ y w):u)
   | x < y = [z,w] ++ (sortByTimeGetText u)
-  | y > x = [w,z] ++ (sortByTimeGetTpext u)
+  | y > x = [w,z] ++ (sortByTimeGetText u)
   | otherwise = [] ++ (sortByTimeGetText u)
 sortByTimeGetText _ = []
 
