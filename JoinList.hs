1c2de93efc58c935ea47c08a7c9e035ba77d2f26
mario <mjh@protonmail.ch>
2015-07-06 17:22:46 -0500

    finished exercise 1 and exercise 2

--- JoinList.hs
+++ JoinList.hs
@@ -11,24 +11,11 @@ data JoinList m a = Empty
                   | Append m (JoinList m a) (JoinList m a)
                   deriving (Eq, Show)
 
---instance Monoid (JoinList m a) where
---  mempty = Empty
---  mappend = (+++)
+test :: JoinList Size String
+test = Append (Size 2) (Single (Size 1) "This is the first full line") (Single (Size 1) "This is the second full line")
 
-test :: JoinList m a
-test = Empty
-
-test2 :: JoinList Size Char
-test2 = Append (Size 210)
-        (Append (Size 30)
-         (Single (Size 5) 'y')
-         (Append (Size 6)
-          (Single (Size 2) 'e')
-          (Single (Size 3) 'a')))
-        (Single (Size 7) 'h')
-
-test3 :: JoinList Size Char
-test3 = Append (Size 2) (Single (Size 1) 'n') (Single (Size 2) 'o')
+test2 :: JoinList Size String
+test2 = Append (Size 4) (Append (Size 3) (Append (Size 2) (Single (Size 1) "First") (Single (Size 1) "Second")) (Single (Size 1) "Third")) (Single (Size 1) "Fourth")
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) Empty x = x
@@ -46,41 +33,25 @@ tag (Append m _ _) = m
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ int _ | int < 0 = Nothing
 indexJ int Empty = Nothing
-indexJ int (Single m a)
-  | (getSize (size m)) == int = Just a
-  | otherwise = Nothing
+indexJ int (Single m a) = Just a
 indexJ int (Append m x y)
-  | (getSize (size (tag y))) == int = indexJ int y
-  | (getSize (size (tag y))) > int = indexJ int x
+  | (getSize (size m)) == int = indexJ int y
+  | otherwise = (indexJ int x)
 
-jlToList :: JoinList m a -> [a]
-jlToList Empty = []
-jlToList (Single _ a) = [a]
-jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
-
-safeListIndex :: [a] -> Int -> Maybe a
-safeListIndex [] _ = Nothing
-safeListIndex _ i | i <= 0 = Nothing
-safeListIndex (x:xs) 1 = Just x
-safeListIndex (x:xs) i = safeListIndex xs (i-1)
-
--- Couldn't make the indices match up; it was a data structure problem
-
-dropJ :: (Sized b, Monoid b) =>
-         Int -> JoinList b a -> JoinList b a
-
-dropJ int jl | int <= 0 = jl
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ int Empty = Empty
-dropJ int (Single m a) = Empty
-dropJ int (Append m (Single x u) y) = dropJ (int - 1) y
+dropJ 0 x = x
+dropJ x (Single m a) | x > 0 = Empty
+dropJ int (Append m x y)
+  | (getSize (size m)) > int = dropJ int x +++ y
+  | otherwise = y
 
-takeJ :: (Sized b, Monoid b) =>
-         Int -> JoinList b a -> JoinList b a
-takeJ int jl | int <= 0 = jl
-takeJ _ Empty = Empty
-takeJ _ (Single m a) = Single m a
-takeJ int (Append m x y) = x +++ (takeJ (int - 1
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ int Empty = Empty
+takeJ 0 x = Empty
+takeJ int (Single m a) | int > 0 = (Single m a)
+takeJ int (Append m x y)
+  | int == (getSize (size m)) = (Append m x y)
+  | int > (getSize (size m)) = Empty
+  | int < (getSize (size m)) = takeJ int x
 
--- Ok...we need to come up with a data structure, specifically, we need to figure
--- out how the JoinLists are structured, and how, or whether, the annotations
--- have any relation to where in the JoinList that particular piece of data is.
