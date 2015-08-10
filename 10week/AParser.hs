24aa5172374bb6621b6df1da3b927e59db12856f
mario <mjh@protonmail.ch>
2015-08-09 21:53:53 -0500

    exercise 1 week 10 done

---
+++ 10week/AParser.hs
@@ -0,0 +1,33 @@
+
+import Control.Applicative
+import Data.Char
+import Data.Maybe
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
+  where
+    f [] = Nothing
+    f (x:xs)
+      | p x = Just (x, xs)
+      | otherwise = Nothing
+
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+posInt :: Parser Integer
+posInt = Parser f
+  where
+    f xs
+      | null ns = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isDigit xs
+
+first :: (a -> b) -> (a,c) -> (b,c)
+first f tuple = ((f (fst tuple)),(snd tuple))
+
+instance Functor Parser where
+  fmap f (Parser g) = (Parser (fmap (first f) . g))
+
+-- JMJ, please help me to understand why this works
