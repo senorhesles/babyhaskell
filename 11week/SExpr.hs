f19cb5539e509b32f83ee5b498d008575f2a0ed6
mario <mjh@protonmail.ch>
2015-08-23 13:09:18 -0500

    started exercise 3

--- 11week/SExpr.hs
+++ 11week/SExpr.hs
@@ -3,6 +3,7 @@ import AParser
 import Control.Applicative
 import Data.Char
 
+-- Exercise 1
 
 zeroOrMore :: Parser a -> Parser [a]
 zeroOrMore p = oneOrMore p <|> pure []
@@ -10,8 +11,18 @@ zeroOrMore p = oneOrMore p <|> pure []
 oneOrMore :: Parser a -> Parser [a]
 oneOrMore p = (:) <$> p <*> zeroOrMore p -- so apparently you can define functions in tandem like this
 
+-- Exercise 2
+
 spaces :: Parser String
 spaces = zeroOrMore . satisfy $ isSpace
 
 ident :: Parser String
 ident = (:) <$> (satisfy isAlpha) <*> (zeroOrMore . satisfy $ isAlphaNum)
+
+-- Exercise 3
+
+
+
+
+
+--JMJ
