9c8ac41b35b2de554f9b394f8b75542cb1920a57
mario <mjh@protonmail.ch>
2015-08-24 23:03:41 -0500

    finished week 11

--- 11week/SExpr.hs
+++ 11week/SExpr.hs
@@ -1,9 +1,30 @@
-
 import AParser
 import Control.Applicative
 import Data.Char
 
+<<<<<<< HEAD
+{-
+parseSExpr :: Parser SExpr
+parseSExpr = Parser f
+  where
+    f xs
+      | null xs = Nothing
+      | 
+-}
+
+test :: Atom
+test = N 5
+test2 :: Atom
+test2 = I "foobar"
+test3 :: SExpr
+test3 = A (N 5)
+test4 :: SExpr
+test4 = Comb [(A (N 5)), (A (I "string"))]
+test5 :: SExpr
+test5 = Comb [(A (N 5)), (A (I "string")), (Comb [(A (N 6)), (A (I "more"))])]
+=======
 -- Exercise 1
+>>>>>>> f19cb5539e509b32f83ee5b498d008575f2a0ed6
 
 zeroOrMore :: Parser a -> Parser [a]
 zeroOrMore p = oneOrMore p <|> pure []
@@ -19,6 +40,20 @@ spaces = zeroOrMore . satisfy $ isSpace
 ident :: Parser String
 ident = (:) <$> (satisfy isAlpha) <*> (zeroOrMore . satisfy $ isAlphaNum)
 
+<<<<<<< HEAD
+parens :: Parser a -> Parser a
+parens p = char '(' *> p <* char ')'
+
+parseAtom :: Parser Atom
+parseAtom = N <$> posInt <|> I <$> ident -- Remember that N and I are type constructors...you can use them before fmap and a parser to get a new parser
+
+parseSExpr :: Parser SExpr
+parseSExpr = spaces *> (A <$> parseAtom <|> Comb <$> (oneOrMore (parens parseSExpr))) <* spaces
+
+type Ident = String
+data Atom = N Integer | I Ident deriving Show
+data SExpr = A Atom | Comb [SExpr] deriving Show
+=======
 -- Exercise 3
 
 
@@ -26,3 +61,4 @@ ident = (:) <$> (satisfy isAlpha) <*> (zeroOrMore . satisfy $ isAlphaNum)
 
 
 --JMJ
+>>>>>>> f19cb5539e509b32f83ee5b498d008575f2a0ed6
