7fb1db896b08ab2ecffb472a90e33bb1c0866047
mario <mjh@protonmail.ch>
2015-09-05 16:42:41 -0500

    made headway with week 12...

--- 12week/Risk.hs
+++ 12week/Risk.hs
@@ -27,11 +27,27 @@ type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
 
-battle :: Battlefield -> Rand StdGen Battlefield
-battle (Battlefield x y) = (Battlefield 
+threeInts :: Rand StdGen (Int,Int,Int)
+threeInts =
+  getRandom >>= \i1 ->
+  getRandom >>= \i2 ->
+  getRandom >>= \i3 ->
+  return (i1,i2,i3)
 
---------------------------------------------------------------
--- Playground
+die1 :: (RandomGen g) => Rand g Int
+die1 = getRandomR (1,6)
 
+dice :: (RandomGen g) => Int -> Rand g [Int]
+dice n = sequence (replicate n die1)
 
-  
+main = do
+  values <- evalRandIO threeInts
+  morevals <- evalRandIO (dice 2)
+  masvals <- evalRandIO die1
+  moredies <- evalRandIO die
+  putStrLn (show values)
+  putStrLn (show morevals)
+  putStrLn (show masvals)
+  putStrLn (show moredies)
+  putStrLn "hello"
+
